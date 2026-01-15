ff9f7f98a2cf1fcd0fbd3abd93959aabed926409
fch <fang.chuhuang@yalla.com>
2026-01-15 13:52:28 +0800

    ignore cs file for format on save

--- lua/plugins/disable-format.lua
+++ lua/plugins/disable-format.lua
@@ -1,9 +1,11 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+--
 return {
   "stevearc/conform.nvim",
   opts = {
     format_on_save = function(bufnr)
-      if vim.bo[bufnr].filetype == "cs" then return false end
-      return true
+      if vim.bo[bufnr].filetype == "cs" then return end
+      return { timeout_ms = 500, lsp_fallback = true }
     end,
   },
 }
