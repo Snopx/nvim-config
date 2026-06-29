9459a2ce709207b554aa4779133670af3ab6b4d2
fch <fang.chuhuang@yalla.com>
2026-06-29 18:33:53 +0800

    move to v6

---
+++ lua/plugins/astroui.lua
@@ -0,0 +1,71 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroUI provides the basis for configuring the AstroNvim User Interface
+-- Configuration documentation can be found with `:h astroui`
+-- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
+--       as this provides autocomplete and documentation while editing
+
+---@type LazySpec
+return {
+  "AstroNvim/astroui",
+  ---@type AstroUIOpts
+  opts = {
+    -- change colorscheme
+    colorscheme = "everforest",
+    -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
+    highlights = {
+      init = { -- this table overrides highlights in all themes
+        -- Normal = { bg = "#000000" },
+        Comment = { fg = "#6e7380", italic = true },
+        Keyword = { italic = true },
+        Function = { italic = true },
+        Identifier = { italic = true },
+        Type = { italic = true },
+        Statement = { italic = true },
+        Conditional = { italic = true },
+        Repeat = { italic = true },
+        Operator = { italic = true },
+        Constant = { italic = true },
+        String = { italic = true },
+
+        -- Treesitter 组
+        ["@comment"] = { italic = true },
+        ["@keyword"] = { italic = true },
+        ["@keyword.function"] = { italic = true },
+        ["@keyword.return"] = { italic = true },
+        ["@conditional"] = { italic = true },
+        ["@repeat"] = { italic = true },
+        ["@function"] = { italic = true },
+        ["@type"] = { italic = true },
+        ["@variable"] = { italic = true },
+        ["@variable.parameter"] = { italic = true },
+
+        -- LSP semantic tokens
+        ["@lsp.type.class"] = { italic = true },
+        ["@lsp.type.enum"] = { italic = true },
+        ["@lsp.type.interface"] = { italic = true },
+        ["@lsp.type.parameter"] = { italic = true },
+        ["@lsp.type.property"] = { italic = true },
+        ["@lsp.type.variable"] = { italic = true },
+        ["@lsp.type.function"] = { italic = true },
+      },
+      astrodark = { -- a table of overrides/changes when applying the astrotheme theme
+        -- Normal = { bg = "#000000" },
+      },
+    },
+    -- Icons can be configured throughout the interface
+    icons = {
+      -- configure the loading of the lsp in the status line
+      LSPLoading1 = "⠋",
+      LSPLoading2 = "⠙",
+      LSPLoading3 = "⠹",
+      LSPLoading4 = "⠸",
+      LSPLoading5 = "⠼",
+      LSPLoading6 = "⠴",
+      LSPLoading7 = "⠦",
+      LSPLoading8 = "⠧",
+      LSPLoading9 = "⠇",
+      LSPLoading10 = "⠏",
+    },
+  },
+}
---
+++ lua/plugins/roslyn.lua
@@ -0,0 +1,61 @@
+return {
+  {
+    "seblyng/roslyn.nvim",
+    opts = {
+      filewatching = "roslyn",
+      broad_search = true,
+      lock_target = false,
+      silent = false,
+    },
+    config = function(_, opts)
+      require("roslyn").setup(opts)
+
+      -- ⭐ AstroNvim v5 的 LSP 注册方式
+      vim.lsp.config("roslyn", {
+        cmd = {
+          "dotnet",
+          "C:\\Roslyn\\Microsoft.CodeAnalysis.LanguageServer.dll",
+          "--stdio",
+        },
+        on_attach = function(client, bufnr)
+          local map = function(mode, lhs, rhs, desc) vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc }) end
+
+          -- ⭐ 你要的跳转键位
+          map("n", "gd", vim.lsp.buf.definition, "Go to definition")
+          map("n", "gD", vim.lsp.buf.declaration, "Go to declaration")
+          map("n", "<Leader>lf", vim.lsp.buf.format, "format buffer")
+          map("n", "<Leader>la", vim.lsp.buf.code_action, "Code Action")
+          -- map("n", "gi", vim.lsp.buf.implementation, "Go to implementation")
+          -- map("n", "gr", vim.lsp.buf.references, "Go to references")
+          -- map("n", "K", vim.lsp.buf.hover, "Hover documentation")
+          map("n", "<leader>rn", vim.lsp.buf.rename, "Rename symbol")
+        end,
+
+        settings = {
+          ["csharp|completion"] = {
+            dotnet_show_completion_items_from_unimported_namespaces = true,
+            dotnet_show_name_completion_suggestions = true,
+          },
+          ["csharp|symbol_search"] = {
+            dotnet_search_reference_assemblies = true,
+          },
+
+          ["csharp|inlay_hints"] = {
+            csharp_enable_inlay_hints_for_implicit_object_creation = true,
+            csharp_enable_inlay_hints_for_implicit_variable_types = true,
+            csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+            csharp_enable_inlay_hints_for_types = true,
+            dotnet_enable_inlay_hints_for_parameters = true,
+          },
+          ["csharp|code_lens"] = {
+            dotnet_enable_references_code_lens = true,
+            dotnet_enable_tests_code_lens = true,
+          },
+          ["csharp|formatting"] = {
+            dotnet_organize_imports_on_format = true,
+          },
+        },
+      })
+    end,
+  },
+}
